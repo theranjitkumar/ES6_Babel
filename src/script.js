10de06160380f86242809bc21b50dc4f938ea612
ranjit <kumarranjitg@gmail.com>
2017-06-01 00:08:02 +0530

    inheritence added

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
+// import User from 'user'
+
 class Es6Test{
+	constructor(){
+		this.name="ranjit"
+	}
     dispName(){
-        console.log('my name is ranjit');
+        console.log(`my name is ${this.name}`);
     }
     dispCity(){
         console.log('my city is patna');
@@ -10,3 +15,12 @@ class Es6Test{
 var et = new Es6Test();
 et.dispName();
 et.dispCity();
+
+class User extends Es6Test {
+	dispContry(){
+		console.log(`contry is india`);
+	}
+}
+var u = new User();
+u.dispCity();
+u.dispContry();
