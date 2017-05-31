822e739bae977f05592278c42e66df92b0b9beb1
theranjitkumar <web.ranjitkumar@gmail.com>
2017-05-31 15:51:50 +0530

    ff

---
+++ src/script.js
@@ -0,0 +1,12 @@
+class Es6Test{
+    dispName(){
+        console.log('my name is ranjit');
+    }
+    dispCity(){
+        console.log('my city is patna');
+    }    
+}
+
+var et = new Es6Test();
+et.dispName();
+et.dispCity();
